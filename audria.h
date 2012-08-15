6dd45669d176d3f3a19d87b56b6285da980f94c9
Alex <alex@SENTEC120.(none)>
2012-08-15 14:04:31 +0200

    use CLOCK_MONOTONIC instead of CLOCK_MONOTONIC_RAW on Linux because of stupid implementations

--- audria.h
+++ audria.h
@@ -10,7 +10,11 @@
 
 // clock source for clock_gettime()
 #if defined(__linux) || defined(__linux__) || defined(linux)
-static const int clockSource = CLOCK_MONOTONIC_RAW; // Linux-specific
+// note: Normally we should use CLOCK_MONOTONIC_RAW on Linux because CLOCK_MONOTONIC is monotonic but
+//       _not_ steady as it can be influenced by NTP. However, clock_nanosleep() doesn't support
+//       CLOCK_MONOTONIC_RAW. Specifying an absolute time obtained by CLOCK_MONOTONIC_RAW will lead
+//       to hickups if both clocks differ, so we have to use the non-steady CLOCK_MONOTONIC instead ;/
+static const int clockSource = CLOCK_MONOTONIC; // Linux-specific
 #else
 static const int clockSource = CLOCK_MONOTONIC;
 #endif
